2e147914f6e7369add9168e619e2313e2dc5daa3
bstoecker <bernhard.stoecker@invision.de>
2013-09-19 16:00:55 +0200

    repaired seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919132538) do
+ActiveRecord::Schema.define(version: 20130919135934) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20130919132538) do
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "teams", force: true do |t|
