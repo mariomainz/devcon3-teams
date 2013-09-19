62aed78576af2770f37cd31b4e3ea2775f4a010c
lasong <Lekealem.Asong@invision.de>
2013-09-19 14:38:56 +0200

    repared mess

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
-<<<<<<< Updated upstream
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :topics
   has_many :responses
-=======
 
->>>>>>> Stashed changes
   has_and_belongs_to_many :teams
 end
