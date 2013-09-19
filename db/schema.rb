a2f68aadedb45e3752409d18f4cf495a411eee72
amclaughlin <Anthony.McLaughlin@invisionwfm.com>
2013-09-19 23:04:45 +0100

    team member now links to profile page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919161730) do
+ActiveRecord::Schema.define(version: 20130919203914) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
