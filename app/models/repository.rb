3d6a5c7a8c51412ab772159475cea893a2bfd508
Mario Mainz <mario.mainz@invision.de>
2013-09-19 15:20:40 +0200

    Added repository controller and views.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,6 @@
 class Repository < ActiveRecord::Base
-
   belongs_to :team
+
+  validates_uniqueness_of :link
+  validates :link, :format => URI::regexp(%w(http https))
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Devcon3Teams::Application.routes.draw do
   devise_for :users
 
   resources :users
+  resources :repositories, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
