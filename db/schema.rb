0313240fe17c8eb88a21eada20991d65ff3154ed
lasong <Lekealem.Asong@invision.de>
2013-09-19 15:59:16 +0200

    added team_id column to repositories and some updates in team

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919131606) do
+ActiveRecord::Schema.define(version: 20130919135706) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20130919131606) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "team_id"
   end
 
   create_table "teams", force: true do |t|
