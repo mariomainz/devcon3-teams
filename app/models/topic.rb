b8ed7dc3af4025e9c2147084486ee93f2728d72f
bstoecker <bernhard.stoecker@invision.de>
2013-09-20 16:27:55 +0200

    fixed form-bug and added some styles

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < Post
   has_many :responses
 
-  scope :recent, -> { order('updated_at DESC') }
+  default_scope order('updated_at DESC')
+  scope :recent, -> { order('updated_at ASC') }
 end
