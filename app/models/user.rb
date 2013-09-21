1d7a40874b4cfd244160c154546598c86cfdf04b
lasong <Lekealem.Asong@invision.de>
2013-09-21 12:25:46 +0200

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :role
+  validates_presence_of :email
+  validates_presence_of :password, on: :create
+
   has_many :topics
   has_many :responses
   has_many :status
