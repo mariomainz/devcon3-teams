e90dd963fa880ad5538cb3f6273b3ba120b6b7fc
bstoecker <bernhard.stoecker@invision.de>
2013-09-21 11:20:46 +0200

    added redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true
+    )
+    x = markdown.render(text).html_safe
+    debugger
+    x
+  end
 end
